d088534f82eda795311cc18a60fc3f594a63051f
Rasheed Aboud <rasheedaboud@gmail.com>
2024-01-03 14:30:31 -0700

    bump

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,19 +1,54 @@
-import { Counter } from "./Counter";
+import { GetinTouch } from "./GetinTOuch";
 import "./index.css";
+import Workingathome from "./workingathome.webp";
 
 export { Page };
 
 function Page() {
   return (
-    <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML. test</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
-    </>
+    <section>
+      <div className='grid auto-rows-max sm:grid-col-3  sm:grid-flow-row lg:grid-rows-3  lg:grid-flow-col sm:gap-4 sm:py-10 '>
+        <div className='py-10  lg:row-span-1 sm:items-center'>
+          <h1 className='text-2xl font-black text-pretty uppercase fade-in-left'>
+            Hey there! 👋 I&apos;m Rasheed!
+          </h1>
+        </div>
+
+        <div className='py-10 lg:row-span-2 text-slate-700 font-semibold fade-in-right'>
+          <p>
+            I&apos;m an experienced developer with a passion for creating
+            innovative solutions. With a strong background in F#, C#, JavaScript
+            (TypeScript), and more, I bring a wealth of knowledge to the table.
+          </p>
+          <br />
+          <p>
+            Specializing in Web development, I take pride in building robust and
+            scalable applications. If you&apos;re in need of a highly skilled
+            developer who can bring your projects to life with a mix of
+            creativity, efficiency, and a touch of finesse, look no further.
+            Let&apos;s work together to create something awesome! 🚀
+          </p>
+        </div>
+        <div className='max-h-1  my-5 fade-in-right'>
+          <GetinTouch />
+        </div>
+
+        <div className='hidden xl:flex xl:row-span-4 slide-in'>
+          <figure className='text-xs text-stone-400'>
+            <img
+              className='object-cover'
+              alt='man working at home looking to book a trip'
+              src={Workingathome}
+            ></img>
+            <figcaption>
+              <a href='https://www.freepik.com/free-vector/man-going-trip-booking-tour-online-guy-sitting-front-computer-table-looking-voyage-summer-vacation_24758680.htm#page=2&query=person%20working%20computer&position=7&from_view=keyword&track=ais&uuid=5c0d4a41-ce76-45d5-b8f7-edb57c64ddae'>
+                Image by studio4rt
+              </a>{" "}
+              on Freepik
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+    </section>
   );
 }
