102a3ca8bccca38543ca87b791a54e6159e07418
rasheedaboud <rasheedaboud@gmail.com>
2024-01-12 16:40:20 -0700

    Update +onBeforePrerenderStart.js

--- pages/blog/@pageTitle/+onBeforePrerenderStart.js
+++ pages/blog/@pageTitle/+onBeforePrerenderStart.js
@@ -1,13 +1,8 @@
 export { onBeforePrerenderStart };
 import { readBlogs } from "../../../types/AzureStorage";
 
-function canonicalize(input: string): string {
-  if(!input) return '';
-  return input.toLowerCase().replace(/\s/g, '-');
-}
-
 async function onBeforePrerenderStart() {
   const blogs = await readBlogs();
-  const blogPageURLs = blogs.map((blog) => "/blog/" + canonicalize(blog.title));
+  const blogPageURLs = blogs.map((blog) => "/blog/" + blog.title.toLowerCase().replace(/\s/g, '-') );
   return blogPageURLs;
 }
