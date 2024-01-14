7b56e4cc7fb6b8cb02ebcef26a8c865a9f5babc5
Rasheed Aboud <rasheedaboud@gmail.com>
2024-01-13 18:54:40 -0700

    bump

--- pages/blog/@pageTitle/+Page.tsx
+++ pages/blog/@pageTitle/+Page.tsx
@@ -5,7 +5,8 @@ import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { usePageContext } from "../../../renderer/usePageContext";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Blog } from "../../../types/Blog";
 
 const CodeCopyBtn = ({ children }: { children: React.ReactElement }) => {
   const [copyOk, setCopyOk] = useState(false);
@@ -37,21 +38,76 @@ const Pre = ({ children }: { children: any }) => (
     {children}
   </pre>
 );
+
+export const Tag = ({ tag }: { tag: string }) => {
+  return (
+    <button
+      key={tag}
+      className='badge badge-primary badge-lg mx-1 text-sm sm:text-base'
+    >
+      {tag}
+    </button>
+  );
+};
+
+const BlogDetailsMobile = (blog: Blog) => {
+  return (
+    <div className='grid-rows-3 grid-flow-col'>
+      <div className='flex justify-center items-center'>
+        {" "}
+        <figure className='max-w-xs px-3 py-2 min-w-xs '>
+          <img
+            style={{ maxWidth: "300px", maxHeight: "300px" }}
+            src={blog.thumbnail}
+            alt='Movie'
+          />
+        </figure>
+      </div>
+
+      <div className='grid grid-rows-4 grid-flow-col gap-0 align-middle items-center sm:px-10'>
+        <div className='avatar placeholder row-span-4 mx-auto'>
+          <div className='bg-neutral text-neutral-content rounded-full w-24'>
+            <span className='text-3xl'>RA</span>
+          </div>
+        </div>
+        <div className='font-bold uppercase mx-5 sm:mx-0'>{blog.author}</div>
+        <div className='uppercase mx-5 lg:mx-0'>{blog.readTime} read</div>
+        <div className='uppercase mx-5 lg:mx-0'>{blog.date}</div>
+        <div className='hidden md:block mx-5 lg:mx-0'>
+          {blog.tags.split(",").map((tag) => (
+            <Tag tag={tag} key={tag} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export { Page };
 function Page() {
   const pageContext = usePageContext();
   const title = pageContext.routeParams?.pageTitle;
-
   const data = useBlobStorage({ title: title });
+
+  useEffect(() => {
+    document.title = data?.title ?? "Rasheed Aboud";
+    const description = document.getElementsByName("description")[0];
+    description.setAttribute(
+      "content",
+      data?.description ?? "Welcome to my personal site."
+    );
+  });
+
   return (
-    <div className='min-h-svh'>
+    <div className='min-h-svh sm:px-10 md:px-24 lg:px-48'>
       <article>
         <div style={{ minWidth: "100% !important" }} className='prose'>
           {data ? (
             <>
-              <h1 className='text-2xl font-black text-pretty uppercase my-5'>
+              <h1 className='text-2xl font-black text-pretty uppercase my-5 text-center'>
                 {data.title}
               </h1>
+              <BlogDetailsMobile {...data} />
               <Markdown
                 remarkPlugins={[remarkGfm]}
                 components={{
--- pages/blog/@pageTitle/+onBeforePrerenderStart.ts
+++ pages/blog/@pageTitle/+onBeforePrerenderStart.ts
@@ -2,7 +2,9 @@ export { onBeforePrerenderStart };
 import { readBlogs } from "../../../types/AzureStorage";
 
 async function onBeforePrerenderStart() {
-  const blogs = await readBlogs();
-  const blogPageURLs = blogs.map((blog) => "/blog/" + blog.title.toLowerCase().replace(/\s/g, '-') );
+  const blogs = (await readBlogs()) ?? [];
+  const blogPageURLs = blogs.map(
+    (blog) => "/blog/" + blog.title.toLowerCase().replace(/\s/g, "-")
+  );
   return blogPageURLs;
 }
