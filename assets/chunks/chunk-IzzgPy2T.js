32878c19fe0a8cbca8cfcd0068a42e3304abd912
rasheedaboud <rasheedaboud@gmail.com>
2024-01-16 14:46:30 +0000

    Deploying to gh-pages from @ rasheedaboud/rasheedaboud.github.io@5c1135472106ff08c61724e4ff031ce46d80137b 🚀

--- assets/chunks/chunk-IzzgPy2T.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./chunk-lqWB3pjr.js";const o=({tag:r,handleTagClick:e})=>s.jsx("button",{className:"badge badge-primary badge-lg mx-1",onClick:()=>e(r),children:r},r);export{o as T};
