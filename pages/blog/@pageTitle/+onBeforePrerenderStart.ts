3ffb5a47423bd8c2f9e251512667dee533bb730b
Rasheed Aboud <rasheedaboud@gmail.com>
2024-06-17 18:20:24 -0600

    added new blog

--- pages/blog/@pageTitle/+onBeforePrerenderStart.ts
+++ pages/blog/@pageTitle/+onBeforePrerenderStart.ts
@@ -1,6 +1,6 @@
 export { onBeforePrerenderStart };
 import { readBlogs } from "../../../types/AzureStorage";
-
+//bump
 async function onBeforePrerenderStart() {
   const blogs = (await readBlogs()) ?? [];
   const blogPageURLs = blogs.map(
